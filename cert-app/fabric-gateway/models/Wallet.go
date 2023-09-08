825a20c08aa06d46eeb1c24a70f59df0b8d58dbd
Dylon Wong <sleepytt1210@gmail.com>
2023-09-09 00:24:07 +0800

    Rename identity to wallet

--- cert-app/fabric-gateway/models/Wallet.go
+++ cert-app/fabric-gateway/models/Wallet.go
@@ -12,9 +12,9 @@ type Tabler interface {
 	TableName() string
 }
 
-type Identity struct {
-	ID         uint64 `gorm:"primaryKey;auto_increment"`
-	Label      string
+type Wallet struct {
+	Id         uint64 `gorm:"primaryKey;auto_increment"`
+	Label      string `gorm:"column:cim_person_id"`
 	MSPID      string
 	PublicKey  []byte
 	PrivateKey []byte
@@ -22,12 +22,12 @@ type Identity struct {
 }
 
 // TableName overrides the table name used by Wallet to `wallet`
-func (Identity) TableName() string {
+func (Wallet) TableName() string {
 	return "wallet"
 }
 
 // Create an X509 certificate object using public key
-func (i *Identity) ToX509Identity() (*identity.X509Identity, error) {
+func (i *Wallet) ToX509Identity() (*identity.X509Identity, error) {
 	cert, err := identity.CertificateFromPEM(i.PublicKey)
 
 	if err != nil {
@@ -38,7 +38,7 @@ func (i *Identity) ToX509Identity() (*identity.X509Identity, error) {
 }
 
 // Create a signer object using private key
-func (i *Identity) ToSign() (identity.Sign, error) {
+func (i *Wallet) ToSign() (identity.Sign, error) {
 	privateKey, err := identity.PrivateKeyFromPEM(i.PrivateKey)
 	if err != nil {
 		return nil, errors.Wrap(err, "Fail to parse private key from wallet's identity")
