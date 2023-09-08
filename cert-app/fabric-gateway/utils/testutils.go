3bf5974838b2589af70b6cc09be7436be902c677
Dylon Wong <sleepytt1210@gmail.com>
2023-09-09 00:23:31 +0800

    Move util files

--- cert-app/fabric-gateway/utils/testutils.go
+++ cert-app/fabric-gateway/utils/testutils.go
@@ -1,4 +1,4 @@
-package service
+package utils
 
 import (
 	"fabric-gateway/config"
--- cert-app/fabric-gateway/utils/utils.go
+++ cert-app/fabric-gateway/utils/utils.go
@@ -1,4 +1,4 @@
-package main
+package utils
 
 // Stops execution if an error is found
 func CheckError(err error) {
