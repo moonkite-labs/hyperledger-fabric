833bc28857917df420ccf49a9778bda56a03889e
Dylon Wong <sleepytt1210@gmail.com>
2023-09-09 16:12:40 +0800

    Simple certificate issuer implemented

--- cert-app/fabric-gateway/service/contract-service.go
+++ cert-app/fabric-gateway/service/contract-service.go
@@ -1,40 +1,37 @@
 package service
 
 import (
-	"crypto/x509"
-
 	"github.com/hyperledger/fabric-gateway/pkg/client"
 	"github.com/hyperledger/fabric-gateway/pkg/identity"
-	"github.com/pkg/errors"
 )
 
 type ContractService struct {
-	gw *client.Gateway
+	gw      *client.Gateway
+	network *client.Network
 }
 
 // TODO: ClaimRequest?
-func (cs ContractService) IssueCertificate(contract *client.Contract, issuerPersonId string, issuerIdentity identity.X509Identity, issuerOrgSigner identity.Sign, masterCertificateId string, issuerHash string, recipientHash string, recipientPublicKey *x509.Certificate, recipientCertId string) (string, error) {
+func (cs *ContractService) IssueCertificate(chaincodeName string, contractName string, issuerPersonId string, issuerOrgSigner identity.Sign, masterCertificateId string, issuerHash string, recipientHash string, recipientId string, recipientPublicKey []byte, recipientCertId string) (string, error) {
 
-	recipientId := recipientPublicKey.Subject.SerialNumber
-
-	rPubBytes, err := identity.CertificateToPEM(recipientPublicKey)
+	// contract := cs.network.GetContractWithName(chaincodeName, contractName)
 
 	// CreateCertificate args: id string, recipientId string, recipientPublicKey []byte, issuerCertHash string, recipientCertHash string
-	txnProposal, err := contract.NewProposal("CreateCertificate", client.WithArguments([]string{masterCertificateId, recipientId}...), client.WithBytesArguments(rPubBytes), client.WithArguments(issuerHash, recipientHash))
-	if err != nil {
-		errors.Wrap(err, "Error creating new proposal to create certificate.")
-		return "", err
-	}
-	txnEndorsed, err := txnProposal.Endorse()
-	if err != nil {
-		errors.Wrap(err, "Error endorsing txn.")
-		return "", err
-	}
-	txnCommitted, err := txnEndorsed.Submit()
-	if err != nil {
-		errors.Wrap(err, "Error submitting transaction:")
-		return "", err
-	}
+	// txnProposal, err := contract.NewProposal("CreateCertificate", client.WithArguments([]string{masterCertificateId, recipientId}...), client.WithBytesArguments(recipientPublicKey), client.WithArguments(issuerHash, recipientHash))
+	// if err != nil {
+	// 	errors.Wrap(err, "Error creating new proposal to create certificate.")
+	// 	return "", err
+	// }
+	// txnEndorsed, err := txnProposal.Endorse()
+	// if err != nil {
+	// 	errors.Wrap(err, "Error endorsing txn.")
+	// 	return "", err
+	// }
+	// txnCommitted, err := txnEndorsed.Submit()
+	// if err != nil {
+	// 	errors.Wrap(err, "Error submitting transaction:")
+	// 	return "", err
+	// }
 
-	return txnCommitted.TransactionID(), nil
+	// return txnCommitted.TransactionID(), nil
+	return "Called", nil
 }
