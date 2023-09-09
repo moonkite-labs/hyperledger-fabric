20ebf91bb0fbf739dc76cb6e9f8ebd2af1632fec
Dylon Wong <sleepytt1210@gmail.com>
2023-09-09 18:33:14 +0800

    Add errors

--- cert-app/gocert/gateway/contract-service.go
+++ cert-app/gocert/gateway/contract-service.go
@@ -3,6 +3,7 @@ package gateway
 import (
 	"github.com/hyperledger/fabric-gateway/pkg/client"
 	"github.com/hyperledger/fabric-gateway/pkg/identity"
+	"github.com/pkg/errors"
 )
 
 type CertContractService struct {
@@ -20,22 +21,22 @@ func NewCertContractService(gw *client.Gateway, channelName string, chaincodeNam
 func (cs *CertContractService) IssueCertificate(issuerPersonId string, issuerOrgSigner identity.Sign, masterCertificateId string, issuerHash string, recipientHash string, recipientId string, recipientPublicKey []byte, recipientCertId string) (string, error) {
 
 	// CreateCertificate args: id string, recipientId string, recipientPublicKey []byte, issuerCertHash string, recipientCertHash string
-	// txnProposal, err := cs.contract.NewProposal("CreateCertificate", client.WithArguments([]string{masterCertificateId, recipientId}...), client.WithBytesArguments(recipientPublicKey), client.WithArguments(issuerHash, recipientHash))
-	// if err != nil {
-	// 	errors.Wrap(err, "Error creating new proposal to create certificate.")
-	// 	return "", err
-	// }
-	// txnEndorsed, err := txnProposal.Endorse()
-	// if err != nil {
-	// 	errors.Wrap(err, "Error endorsing txn.")
-	// 	return "", err
-	// }
-	// txnCommitted, err := txnEndorsed.Submit()
-	// if err != nil {
-	// 	errors.Wrap(err, "Error submitting transaction:")
-	// 	return "", err
-	// }
+	txnProposal, err := cs.contract.NewProposal("CreateCertificate", client.WithArguments([]string{masterCertificateId, recipientId}...), client.WithBytesArguments(recipientPublicKey), client.WithArguments(issuerHash, recipientHash))
+	if err != nil {
+		errors.Wrap(err, "Error creating new proposal to create certificate.")
+		return "", err
+	}
+	txnEndorsed, err := txnProposal.Endorse()
+	if err != nil {
+		errors.Wrap(err, "Error endorsing txn.")
+		return "", err
+	}
+	txnCommitted, err := txnEndorsed.Submit()
+	if err != nil {
+		errors.Wrap(err, "Error submitting transaction:")
+		return "", err
+	}
 
-	// return txnCommitted.TransactionID(), nil
-	return "Called", nil
+	return txnCommitted.TransactionID(), nil
+	// return "Called", nil
 }
